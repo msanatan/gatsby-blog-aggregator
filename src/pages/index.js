b9bece69a7a60ebae729a0a561e3d67f04ad708f
Marcus Sanatan <msanatan@gmail.com>
2021-07-07 07:43:45 -0400

    Style page and cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { Container, Row, Col, Nav, Card, Button } from 'react-bootstrap'
 import { graphql } from 'gatsby'
+import { pageTitle, cardTitle, cardBody, card, cardButton } from './index.module.css'
 
 const IndexPage = ({ data }) => {
   console.log(data)
@@ -12,15 +13,15 @@ const IndexPage = ({ data }) => {
     const postCols = postChunk.map((post, index) => {
 
       return (
-        < Col sm='4' key={post.ID} className='d-flex justify-content-center' >
-          <Card style={{ width: '18rem' }} className='my-3'>
-            <Card.Body>
-              <Card.Title>{post.title}</Card.Title>
-              <Card.Text>{post.description}</Card.Text>
-              <Button variant='primary' as='a' href={post.link} target='_blank' rel='noopener nofollow noreferrer'>Read More</Button>
+        <Col sm='4' key={post.ID} className='d-flex justify-content-center' >
+          <Card style={{ width: '18rem' }} className={`my-3 ${card}`}>
+            <Card.Body className='d-flex flex-column'>
+              <Card.Title className={cardTitle}>{post.title}</Card.Title>
+              <Card.Text className={cardBody}>{post.description}</Card.Text>
+              <Button variant='primary' as='a' href={post.link} target='_blank' rel='noopener nofollow noreferrer' className={`mt-auto ${cardButton}`}>Read More</Button>
             </Card.Body>
           </Card>
-        </Col >
+        </Col>
       )
     })
 
@@ -33,7 +34,9 @@ const IndexPage = ({ data }) => {
         <Col sm={8}>
           <Nav className='justify-content-center' activeKey='/home'>
             <Nav.Item>
-              <Nav.Link href='/home'>Irie Tech Blogs</Nav.Link>
+              <Nav.Link href='/home'>
+                <h1 className={`${pageTitle}`}>Irie Tech Blogs</h1>
+              </Nav.Link>
             </Nav.Item>
           </Nav>
         </Col>
