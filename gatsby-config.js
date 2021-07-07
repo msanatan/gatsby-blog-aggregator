7858c5ea85f08e2e72508a6a119126136ff05bc1
Marcus Sanatan <msanatan@gmail.com>
2021-07-07 00:39:26 -0400

    Add correct REST API wrapper
    Works like a charm

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,50 +1,80 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Gatsby Blog Aggregator",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Gatsby Blog Aggregator',
   },
   plugins: [
-    "gatsby-plugin-image",
+    'gatsby-plugin-image',
+    // {
+    //   resolve: 'gatsby-plugin-google-analytics',
+    //   options: {
+    //     trackingId: '',
+    //   },
+    // },
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        trackingId: "",
+        icon: 'src/images/icon.png',
       },
     },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        icon: "src/images/icon.png",
+        name: 'images',
+        path: './src/images/',
       },
-    },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
+      __key: 'images',
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/',
       },
-      __key: "pages",
+      __key: 'pages',
     },
     {
-      resolve: 'gatsby-source-rest-api',
+      resolve: 'gatsby-source-custom-api',
       options: {
-        endpoints: [
-          'http://localhost:8080'
-        ],
+        url: 'http://localhost:8080/posts'
       },
+      rootKey: 'getPosts',
+      schemas: {
+        getPosts: `
+          limit: Int
+          total: Int
+          page: Int
+          items: [posts]
+        `,
+        posts: `
+          CreatedAt: Date
+          UpdatedAt: Date
+          DeletedAt: Date
+          ID: ID
+          title: String
+          description: String
+          content: String
+          link: String
+          blogId: String
+          categories: [category]
+          published: String
+          PublishDate: Date
+          views: Int
+        `,
+        category: `
+          ID: Int
+          CreatedAt: Date
+          UpdatedAt: Date
+          DeletedAt: Date
+          Title: String
+          Description: String
+        `
+      }
     },
   ],
 };
